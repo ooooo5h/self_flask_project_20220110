7284460531e2fef6f1d6318b710cabc9b02de16b
ooooo5h <ooooo5h91@gmail.com>
2022-01-10 12:47:55 +0900

    개인정보를 담는 dict 생성
    - JSON형태의 응답으로 내보낼 예정

--- app/app.py
+++ app/app.py
@@ -20,5 +20,16 @@ def create_app():
     def web_test():
         return render_template('web_test.html')   # templates폴더 내부의 파일을 불러내는 코드
     
+    
+    @app.route("/json")
+    def json_test():
+        
+        # JSON양식 => "이름표" : 실제값 의 조합(딕셔너리를 이용하면 작업이 편함)
+        test_dict = {}
+        test_dict['menu'] = '빵',
+        test_dict['price'] = 3500,
+        test_dict['store_name'] = '맛있는 빵집',
+        test_dict['is_open'] = True
+    
     # 이 서버를 사용하도록 결과로 리턴하는 부분
     return app
