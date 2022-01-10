51fc469f8ca80c00c4a91aa8ec02257a6d6acf56
ooooo5h <ooooo5h91@gmail.com>
2022-01-10 12:35:56 +0900

    주석 추가함

--- app/app.py
+++ app/app.py
@@ -1,5 +1,12 @@
 from flask import Flask
 
 def create_app():
+    # 플라스크 서버를 변수에 담았음
     app = Flask(__name__)
+    
+    # 서버에 대한 세팅 진행하는 부분
+    
+    
+    
+    # 이 서버를 사용하도록 결과로 리턴하는 부분
     return app
