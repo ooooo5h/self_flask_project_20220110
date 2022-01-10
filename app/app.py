bb4ae73f07dc2c4b42462c309fdd57ebcff0f645
ooooo5h <ooooo5h91@gmail.com>
2022-01-10 12:39:51 +0900

    http://192.168.0.37:5000/로 가면, 내가 쓴 문장을 보여줌

--- app/app.py
+++ app/app.py
@@ -5,8 +5,9 @@ def create_app():
     app = Flask(__name__)
     
     # 서버에 대한 세팅 진행하는 부분
-    
-    
+    @app.route("/")
+    def test():
+        return "<h1>나혼자 도전</h1>"
     
     # 이 서버를 사용하도록 결과로 리턴하는 부분
     return app
