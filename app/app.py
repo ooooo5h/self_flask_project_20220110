e878d1e300435d5ecd65f5c297f04bd6ee795e33
ooooo5h <ooooo5h91@gmail.com>
2022-01-10 12:56:44 +0900

    코드 구조 변경 관련 주석 추가

--- app/app.py
+++ app/app.py
@@ -14,53 +14,12 @@ def create_app():
         return "<h1>나혼자 도전</h1>"
     
     
-    @app.route("/test")
-    def test():
-        return "여긴 테스트"
-    
-    
-    @app.route("/web")
-    def web_test():
-        return render_template('web_test.html')   # templates폴더 내부의 파일을 불러내는 코드
-    
-    
-    @app.route("/json")
-    def json_test():
-        
-        # JSON양식 => "이름표" : 실제값 의 조합(딕셔너리를 이용하면 작업이 편함)
-        test_dict = {}
-        test_dict['menu'] = '빵',
-        test_dict['price'] = 3500,
-        test_dict['store_name'] = '맛있는 빵집',
-        test_dict['is_open'] = True
-        
-        return jsonify(test_dict)
-    
-    
-    @app.route("/json2")
-    def json_test2():
-        hello_dict = {}
-        hello_dict['korean'] = '배고파요'
-        hello_dict['english'] = 'i am hungry'
-        
-        return hello_dict
-    
-    @app.route("/json3")
-    def json_test3():
-        user_dict = {}
-        user_dict['name'] = '전은형'
-        user_dict['address'] = '서울시 중랑구'
-        
-        return {
-            'code' : 200,
-            'message' : '성공',
-            'data' : user_dict
-        }
-    
-    
     @app.route('/module_test')
     def module_test():
         return user_test()
+        # 다른 모듈의 함수 실행결과를 내보내자
+        # 로직은 다른 모듈에서만 작성하고
+        # main.py에서는 불러내기만 실행할 예정
 
     # 이 서버를 사용하도록 결과로 리턴하는 부분
     return app
